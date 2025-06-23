3e7ab4aade6d5ba0cd9decf962405753ff2ef249
Georgi Roussos <georgieann14@gmail.com>
2025-06-23 15:38:17 +0200

    R plot exploration

---
+++ Proj_exploration.R
@@ -0,0 +1,24 @@
+
+df_C <- read_excel("/Users/georgiaroussos/Desktop/MMED_C.xlsx", skip = 1) %>% 
+  mutate(prop_infected = Infected/dissected, 
+         species = "Congolense")
+
+df_V <- read_excel("/Users/georgiaroussos/Desktop/MMED_V.xlsx", skip = 1) %>% 
+  mutate(prop_infected = Infected/dissected, 
+         species = "Vivax")
+
+df_comb <- rbind(df_C, df_V)
+
+ggplot() +
+  geom_line(data = df_comb, aes(x = category, y = prop_infected, group = species),
+            color = "black", alpha = 0.2) +
+  geom_point(data = df_comb, aes(x = category, y = prop_infected, group = species, color = species),
+             size = 2,  fshape = 21) +
+  #scale_color_brewer(palette = "Paired") +   # Color scale for lines, points, and error bars
+  #scale_fill_brewer(palette = "Paired") + 
+  labs(title = "Proportion of Tstetse Flies Infected",
+       x = "Age Category (t)",
+       y = "Proportion Infected", 
+       color = "Species") +
+  theme_bw()
+
